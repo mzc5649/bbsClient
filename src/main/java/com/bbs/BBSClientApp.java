69e2254bfdd26c40183787a3b45d001d856e319a
mzc5649 <13027087095@163.com>
2020-10-12 11:07:48 +0800

    first commit

---
+++ src/main/java/com/bbs/BBSClientApp.java
@@ -0,0 +1,18 @@
+package com.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ *
+ * @Author: 马志朝
+ * @Date: 2020/9/29 15:11
+ */
+
+@SpringBootApplication
+public class BBSClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BBSClientApp.class, args);
+    }
+}
